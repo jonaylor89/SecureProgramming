a7524bdea34bb2b804b95cff8684849c4971d575
John Naylor <jonaylor89@gmail.com>
2017-12-13 14:07:26 -0500

    Sanitizers for files

--- sanitizers.c
+++ sanitizers.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <paths.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 #include "sanitizers.h"
 
 /* These arrays are both null terminated */
@@ -17,6 +22,16 @@ static char *preserve_environ[] = {
 	"TZ",
 	0
 };
+
+static int open_devnull(int fd){
+	FILE *f = 0;
+
+	if(!fd) f = freopen(_PATH_DEVNULL, "rb", stdin);
+	else if(fd == 1) f = freopen(_PATH_DEVNULL, "wb", stdout);
+	else if(fd == 2) f = freopen(_PATH_DEVNULL, "wb", stderr);
+
+	return (f && fileno(f) == fd);
+}
 
 void sanitize_environment(int preservec, char **preservev){
 	int i;
@@ -77,4 +92,20 @@ void sanitize_environment(int preservec, char **preservev){
 
 	environ = new_environ;
 }
+
+void sanitize_files(void){
+	int fd, fds;
+	struct stat st;
+
+	// Ensure all file descriptors other than standard ones are closed
+	if((fds = getdtablesize()) == -1) fds = OPEN_MAX;
+
+	for(fd = 3; fd < fds; fd++) close(fd);
+
+	// Ensure standard file descriptors are open
+	for(fd = 3; fd < 3; fd++){
+		if(fstat(fd, &st) == -1 && (errno != EBADF || !open_devnull(fd))) abort;
+	}
+}
+
 
--- sanitizers.h
+++ sanitizers.h
@@ -1,8 +1,13 @@
 #ifndef _sanitizers_h
 #define _sanitizers_h 
 
+#ifndef OPEN_MAX
+#define OPEN_MAX 256
+#endif
+
 extern char **environ;
 
 void sanitize_environment(int preservec, char **preservev);
+void sanitize_files(void);
 
 #endif
